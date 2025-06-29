fa307c6f7fd8ea99736464b42e48e13a60296b36
sayedAli <said.ali199859@gmail.com>
2025-06-29 09:14:20 +0300

    added heart_transcation_binding withMoive

--- src/app/componants/movie-card/movie-card.ts
+++ src/app/componants/movie-card/movie-card.ts
@@ -2,6 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { MovieService } from '../../shared/movie.service';
 import { CommonModule } from '@angular/common';
 
+declare const AOS: any; // Add this if you get a TS error about AOS
 
 @Component({
   selector: 'app-movie-card',
@@ -9,15 +10,19 @@ import { CommonModule } from '@angular/common';
   templateUrl: './movie-card.html',
   styleUrl: './movie-card.css'
 })
-export class MovieCard  {
-
+export class MovieCard implements OnInit {
   movieService = inject(MovieService);
+  isFavorite = false;
 
   constructor() {
-     this.movieService.movieId.set(11); // To change the movie
+    this.movieService.movieId.set(11); // To change the movie
   }
 
-   
-   
-  
+  ngOnInit() {
+    AOS.init(); // Initialize AOS for animations
+  }
+
+  toggleFavorite() {
+    this.isFavorite = !this.isFavorite;
+  }
 }
