862b790f55ef66850df177e8563122cc9c1d885e
abdelrahmanabdelsabour <abdoabdelsabour355@gmail.com>
2025-07-02 14:47:09 +0200

    add login and regist

---
+++ src/app/componants/register/register.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './register.html',
+})
+export class RegisterComponent {
+  registerForm: FormGroup;
+  submitted = false;
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required]],
+      phone: ['', [Validators.required, Validators.pattern("^[0-9]{11}$")]]
+    }, { validators: this.passwordMatchValidator });
+  }
+
+  passwordMatchValidator(form: AbstractControl) {
+    const pass = form.get('password')?.value;
+    const confirm = form.get('confirmPassword')?.value;
+    if (pass !== confirm) {
+      form.get('confirmPassword')?.setErrors({ mismatch: true });
+    } else {
+      form.get('confirmPassword')?.setErrors(null);
+    }
+    return null;
+  }
+
+  get f() {
+    return this.registerForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.registerForm.invalid) return;
+    console.log("✅ نموذج التسجيل:", this.registerForm.value);
+  }
+}
