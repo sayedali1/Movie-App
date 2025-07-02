7247c219e3a360c44e04a3e758c100bb14472371
abdelrahmanabdelsabour <abdoabdelsabour355@gmail.com>
2025-07-02 14:45:25 +0200

    k

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,11 @@ import { Routes } from '@angular/router';
 import { MovieCard } from './componants/movie-card/movie-card';
 import { App } from './app';
 import { HomeComponant } from './componants/home-componant/home-componant';
-import { Wishlist } from './componants/wishlist/wishlist';
 
 import { SearchResult } from './componants/search-result/search-result';
 import { Allgenre } from './componants/allgenre/allgenre';
 export const routes: Routes = [
   
     { path: '', component: HomeComponant },
-    { path: 'wishlist', component: Wishlist },
-  { path: '', component: HomeComponant, title: 'Home' },
-  { path: 'movies', component: Allgenre, title: 'movies' },
-  { path: 'search', component: SearchResult, title: 'search-Result' }
+    { path: 'movies', component: MovieCard },
 ];
