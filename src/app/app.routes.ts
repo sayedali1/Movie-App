8874ae556d488c971d82fb0e7a8615b6f89519cf
sayedAli <said.ali199859@gmail.com>
2025-07-03 06:01:22 +0300

    rout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,23 +5,18 @@ import { HomeComponant } from './componants/home-componant/home-componant';
 
 import { SearchResult } from './componants/search-result/search-result';
 import { Allgenre } from './componants/allgenre/allgenre';
-
+import { GenreMovies } from './componants/genre-movies/genre-movies';
 import { RegisterComponent } from './componants/register/register';
 import { LoginComponent } from './componants/login/login';
-export const routes: Routes = [
-  
- 
-        { path: 'register', component: RegisterComponent },
-                { path: 'login', component: LoginComponent },
-
-import { GenreMovies } from './componants/genre-movies/genre-movies';
+import { Wishlist } from './componants/wishlist/wishlist';
 export const routes: Routes = [
   
     { path: '', component: HomeComponant },
     { path: 'wishlist', component: Wishlist },
+    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LoginComponent },
   { path: '', component: HomeComponant, title: 'Home' },
   { path: 'movies', component: Allgenre, title: 'movies' },
-
   { path: 'search', component: SearchResult, title: 'search-Result' },
   {
     path: 'genre',
